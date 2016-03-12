a78b7c127c0cd043ecd8843db066b07fb136ef59
Nick Schmidt <nick.schmidt3@gmail.com>
2016-03-11 19:46:54 -0900

    Add install.py file

---
+++ install.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# *** Installation Script ***
+# Nick Schmidt, 11 Mar 2016
+# Ideally, this will install all of the things so that they run nicely without much intervention
+# TODO
+# Verify if SU
+# Add Symlinks
+# Actually Install something
